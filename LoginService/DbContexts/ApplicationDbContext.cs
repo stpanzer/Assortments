5abab95a04800c3e80bffd61c9ed68db1198c4e4
Stephen Panzer <stephen@panzercode.com>
2020-07-16 13:49:39 -0600

    Add identity packages, first migration, some startup work

---
+++ LoginService/DbContexts/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    public ApplicationDbContext(DbContextOptions options) : base(options)
+    {
+    }
+}
